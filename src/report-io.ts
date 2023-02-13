4b50e713372c6cccfae0458b012023a57b11a825
nbe <aleisa.naser@gmail.com>
2023-02-13 03:59:10 +0300

    abort early if user aborts test run

--- src/report-io.ts
+++ src/report-io.ts
@@ -1,20 +1,13 @@
 import type * as P from '@playwright/test/reporter';
-import type { StrategyResult } from 'neotest';
 import * as lib from 'neotest.lib';
 import * as logger from 'neotest.logging';
 
-export const readReport = (file: string, result?: StrategyResult) => {
+export const readReport = (file: string) => {
 	const [success, data] = pcall(lib.files.read, file);
 
 	if (!success) {
-		if (result?.code === 129) {
-			// Code 129: User stopped the test run
-			logger.debug('Code 129: User stopped the test run');
-			return {};
-		} else {
-			logger.error('No test output file found', file);
-			throw new Error('No test output file found');
-		}
+		logger.error('No test output file found', file);
+		throw new Error('No test output file found');
 	}
 
 	const [ok, parsed] = pcall(vim.json.decode, data, {
@@ -23,7 +16,7 @@ export const readReport = (file: string, result?: StrategyResult) => {
 
 	if (!ok) {
 		logger.error('Failed to parse test output json', file);
-		return null;
+		throw new Error('Failed to parse test output json');
 	}
 
 	return parsed as P.JSONReport;
--- src/results.ts
+++ src/results.ts
@@ -1,13 +1,19 @@
 import { parseOutput } from 'neotest-playwright/report';
+import * as logger from 'neotest.logging';
 import { readReport } from './report-io';
 import type { Adapter } from './types/adapter';
 
 export const results = ((spec, result, _tree) => {
+	if (result.code === 129) {
+		// Code 129: User stopped the test run
+		logger.debug('Code 129: User stopped the test run. Aborting result parse');
+		return {};
+	}
+
 	const resultsPath = spec.context.results_path;
 
-	const decoded = readReport(resultsPath, result);
+	const decoded = readReport(resultsPath);
 
-	// @ts-expect-error throw?
 	const results = parseOutput(decoded);
 
 	return results;
