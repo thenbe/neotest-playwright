25f8b91b499cbe80659902eb04149364430c7ca6
Naser Aleisa <aleisa.naser@gmail.com>
2024-06-03 18:20:29 +0300

    chore: add pre-commit hook for formatting

--- flake.nix
+++ flake.nix
@@ -2,9 +2,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, ... } @ inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -12,9 +13,18 @@
       {
         devShells = {
           default = pkgs.mkShell {
-            packages = with pkgs; [
-              stylua
-            ];
+            inherit (self.checks.${system}.pre-commit-check) shellHook;
+            buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
+            # packages = with pkgs; [ ];
+          };
+        };
+        checks = {
+          pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              nixpkgs-fmt.enable = true;
+              stylua.enable = true;
+            };
           };
         };
       }
